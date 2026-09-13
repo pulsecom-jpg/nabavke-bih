3fd89465919066aa2c4935af40b6596ea2b1aa7f
pulsecom-jpg <pulse.com@gmail.com>
2026-09-13 17:03:12 +0200

    Delete schema.sql

--- schema.sql
+++
@@ -1,2 +0,0 @@
--- Schema je u init() funkciji app.py; ova datoteka sluzi kao kratka referenca.
--- Glavna tabela: ugovori; dimenzije su namjerno genericke da ista aplikacija podrzi bilo koju vrstu javne nabavke.
